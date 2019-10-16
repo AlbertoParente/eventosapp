aa67ffa4ce72503ea3c548416bbf6a3313cacc50
AlbertoParente <albertoparente20@gmail.com>
2019-10-16 11:06:35 -0300

    Add event list

--- src/main/java/com/eventosapp/models/Evento.java
+++ src/main/java/com/eventosapp/models/Evento.java
@@ -6,14 +6,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javassist.SerialVersionUID;
+//import javassist.SerialVersionUID;
 
 
 
 @Entity
 public class Evento implements Serializable{
 	
-	private static final long SerialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy= GenerationType.AUTO)
