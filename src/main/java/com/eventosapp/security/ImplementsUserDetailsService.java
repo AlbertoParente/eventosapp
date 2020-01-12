74055f7617e842eb9bccc5cd1a97cc9d2a23ffd7
AlbertoParente <albertoparente20@gmail.com>
2020-01-12 20:15:42 -0300

    Creating Model Usuario and UsuarioRepository and class ImplementsUserDetailsService for user validation using JPA

---
+++ src/main/java/com/eventosapp/security/ImplementsUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.eventosapp.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Repository;
+
+import com.eventosapp.models.Usuario;
+import com.eventosapp.repository.UsuarioRepository;
+
+@Repository
+public class ImplementsUserDetailsService implements UserDetailsService{
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+		Usuario usuario = usuarioRepository.findByLogin(login);
+		
+		if(usuario == null) {
+			throw new UsernameNotFoundException("Usuario não encontrado!");
+		}
+		return usuario;
+	}
+}
